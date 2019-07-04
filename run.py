9026d3e533d7774f2356183ecad0d4f4ff500883
Rusmana-ID <41493567+Rusmana-ID@users.noreply.github.com>
2019-07-04 08:49:48 +0700

    Add files via upload

---
+++ run.py
@@ -0,0 +1,10 @@
+import os, time
+
+os.system("clear")
+os.system("pkg install python2 -y")
+os.system("pkg install unzip")
+os.system("pip2 install requests")
+os.system("pip2 install mechanize")
+os.system("pip2 install bs4")
+os.system("clear")
+print("Penginstallan Telah Selesai")
